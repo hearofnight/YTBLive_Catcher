aa43691f415a61e47674864556a5a5f13fec508e
hearofnight <63850765+hearofnight@users.noreply.github.com>
2024-01-15 17:38:32 +0800

    Update y2blive.sh

--- y2blive.sh
+++ y2blive.sh
@@ -7,7 +7,7 @@ CHANNELS=($(curl -s "$CHANNEL_URL"))
 # 遍历每个频道
 for channel in "${CHANNELS[@]}"; do
     # 调用 xxx.py 脚本并传递频道参数
-    python3 SingleChannelAnalysis.py "$channel" > "channels/$channel.m3u8"
+    python3 scripts/SingleChannelAnalysis.py "$channel" > "channels/$channel.m3u8"
 done
 #保存执行日志到logs/youtube_live_`date +%Y%m%d`.log
 date +'Última captura de links foi %A, %d de %B de %Y às %H:%M' >> logs/youtube_live_`date +%Y%m%d`.log
