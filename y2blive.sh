a25488eea16d0d2118e654a98827b3f448940118
hearofnight <63850765+hearofnight@users.noreply.github.com>
2024-01-15 18:15:02 +0800

    Update y2blive.sh

--- y2blive.sh
+++ y2blive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# 设置时区为中国香港
+export TZ=Asia/Hong_Kong
 # 从URL读取频道列表
 CHANNEL_URL="https://raw.githubusercontent.com/hearofnight/YTBLive_Catcher/main/sources/ChannelNeed.txt"
 CHANNELS=($(curl -s "$CHANNEL_URL"))
@@ -12,7 +13,7 @@ for channel in "${CHANNELS[@]}"; do
     python3 scripts/SingleChannelAnalysis.py "$channel" > "newchannels/$channel.m3u8"
 done
 #保存执行日志到logs/youtube_live_`date +%Y%m%d`.log
-date +'Última captura de links foi %A, %d de %B de %Y às %H:%M' >> logs/youtube_live_`date +%Y%m%d`.log
+date +'最后一次链接捕获时间为 %Y 年 %m 月 %d 日%A %H:%M:%S' >> logs/youtube_live_`date +%Y%m%d`.log
 #Log de Capturas
 #date +'Última captura de links foi %A, %d de %B de %Y às %H:%M' >> captures.log
 
